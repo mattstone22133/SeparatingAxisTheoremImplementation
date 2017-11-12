58c06449af7d768e8d3217e58d35adba001e4592
Matt Stone <mattstone22133@gmail.com>
2017-11-12 15:08:34 -0500

    updated libgdx project that has better project names

---
+++ SAT/desktop/src/sat/simulation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sat.simulation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sat.simulation.SATMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SATMain(), config);
+	}
+}
